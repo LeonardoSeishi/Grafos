c46e013bc68fd6f3f3641d287e9a6371d2be0773
victor.valle <victorvcunha@hotmail.com>
2022-09-19 14:06:42 -0300

    funcoes com dicionario e main

--- grafos.py
+++ grafos.py
@@ -1,34 +1,75 @@
-#biblioteca de grafos
+class Graph:
+	# Por enquanto, o array de vertices está sendo inicializado
+	# com None como primeiro elemento pois os vértices dos arquivos de 
+	# texto iniciam pelo número 1.
+	def __init__(self):
+		self.__adj = [None] # array de vertices
 
-class Grafo_nD_P: #grafo nao dirigido e ponderado
-    def __init__(self, V, E, w ):
-        self.V = V  #conjunto de vertices
-        self.E = E  #conjunto de arestas
-        self.w = w  #função que mapeia o peso de cada aresta {u, v} ∈ E
+	@property
+	def adj(self):
+			return self.__adj
 
-    def qtdVertices(self):
-        return len(self.V)
+	# Complexidade O(1)
+	# TODO: Retirar o menos um se nao tiver o NoneType como primeiro elemento
+	def qtdVertices(self):
+		return len(self.__adj) - 1
 
-    def qtdArestas(self):
-        return len(self.E)
+    # Complexidade O(1)
+	# TODO: Mudar o começo do for de 1 para zero se nao tiver o NoneType como primeiro elemento
+	def qtdArestas(self):
+		qnt = 0
+		for v in range (1, len(self.__adj)):	
+			qnt += len(self.__adj[v]["arestas"])
+		return qnt / 2
 
-    def grau(self, v):
-        return 0
+    # # Numero de vizinhos
+	def grau(self, v):
+		return len(self.__adj[v]["arestas"])
 
-    def rotulo(self, v):
-        return 0
+	def rotulo(self, v):
+		return self.__adj[v]["label"]
 
-    def vizinhos(self, v):
-        return []
+	# TODO : printar o nome dos usuários
+	def vizinhos(self, v):
+		return self.__adj[v]["arestas"]
 
-    def haAresta(self, u, v):
-        return False
+	def haAresta(self, u, v):
+		for vv in self.__adj:
+			if vv["arestas"] == v:
+				return True
+		return False
+		
+	def peso(self, u, v):
+		return self.__adj[u]["arestas"][v]
 
-    def peso(self, u, v):
-        return 999999999999999
+	# ? O primeiro elemento a ser inserido na lista é NoneType. está correto?
+	def ler(self, file_path):
+		try:
+			file = open(file_path, 'r')
+		except FileNotFoundError:
+			print("Arquivo nao encontrado")
 
-    def ler(self, arquivo):
-        return 0
+		try:
+			number_vertices = int(file.readline()[10:-1])
+		except ValueError:
+			print("Nao foi possivel obter numero de vertices")
 
-    
+		for line in range(number_vertices):
+			vertice = {"label": "", "size": 0, "arestas": {}}
+			line = file.readline()
+			words = line.split("\"") # funciona enquanto os rotulos estiverem dentro de aspas duplas
+			vertice["label"] = words[1]
+			self.__adj.append(vertice)
+
+		file.readline() # para pular a linha "*edges"
+
+		while True:
+			line = file.readline()
+			if not line:
+				break
+			vert1, vert2, weight = line.split()
+			vert1 = int(vert1)
+			weight = float(weight)
+			self.__adj[vert1]["arestas"][vert2] = weight
+
 
---
+++ main.py
@@ -0,0 +1,28 @@
+from grafos import *
+import sys
+
+class Main:
+    grafo = Graph()
+
+    # Caso queira passar o path do arquivo como argumento pela linha de comando.
+    # Senao, por enquanto, é só alterar o path aqui no else
+    if (len(sys.argv) > 1):
+        file_path = sys.argv[1]
+    else:
+        file_path = "instancias/arvore_geradora_minima/agm_tiny.net"
+
+    grafo.ler(file_path)
+
+    # imprime o array de vertices
+    print(grafo.adj[0])
+    for i in grafo.adj:
+        print(i)
+    
+    #print(grafo.qtdVertices())
+    print(grafo.qtdArestas())
+    # print(grafo.grau())
+    # print(grafo.rotulo())
+    # print(grafo.vizinhos())
+    # print(grafo.haAresta())
+    # print(grafo.peso())
+    
