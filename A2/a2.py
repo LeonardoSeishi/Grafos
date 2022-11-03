78926bf854b87dc034cabcf32fcb280aee37781b
c.alchini <alchinicristian@gmail.com>
2022-11-03 19:57:49 -0300

    final cfc

--- A2/a2.py
+++ A2/a2.py
@@ -1,7 +1,8 @@
-from cfc import *
-from grafos import *
 from ordenacao_topologica import visit_ot, print_ord
+from grafos import *
+from cfc import *
 import sys
+sys.path.insert(0, "../Grafos")
 # sys.path.insert(0, "/home/vvc/Desktop/A1 Project/Grafos/Grafos")
 
 
@@ -13,9 +14,13 @@ class A2:
     if (len(sys.argv) > 1):
         file_path = sys.argv[1]
     else:
+        # file_path = "../instancias/dirigidos/dirigido2.net"
         # file_path = "../instancias/dirigidos/dirigido1.net"
+        # file_path = "../instancias/dirigidos/simpsons_amizades1.net"
+        # file_path = "../instancias/dirigidos/tcc_completo.net"
+        file_path = "../instancias/dirigidos/manha.net"
         # file_path = "../instancias/teste_locais/abcd.net"
-        file_path = "../instancias/teste_locais/componentes.net"
+        # file_path = "../instancias/teste_locais/componentes.net"
 
     grafo.ler(file_path)
 
--- A2/ordenacao_topologica.py
+++ A2/ordenacao_topologica.py
@@ -1,6 +1,6 @@
 import sys
 
-sys.path.insert(0, "../Grafos/Grafos")
+sys.path.insert(0, "../Grafos")
 
 from grafos import *
 
